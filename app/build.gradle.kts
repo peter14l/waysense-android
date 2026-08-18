d5047425e4bdb6500a98bd4e9897bac5d4563f02
peter14l <peter14l@users.noreply.github.com>
2026-08-19 00:19:39 +0530

    feat: enable ABI splits for arm64-v8a, armeabi-v7a, x86_64

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,15 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("arm64-v8a", "armeabi-v7a", "x86_64")
+            isUniversalApk = false
+        }
+    }
 }
 
 dependencies {
